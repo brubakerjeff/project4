778286e0f2a0fb0923578dd4c7997db85445722f
Your Name <you@example.com>
2025-07-26 19:14:55 -0700

    Updates

---
+++ src/scripts/pick_objects.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ..
+# Launch the robot simulation
+roslaunch turtlebot_gazebo turtlebot_world.launch &
+
+# Launch the AMCL node for localization
+roslaunch turtlebot_navigation amcl.launch map_file:=/home/robond/catkin_ws/src/map/map.yaml &
+
+# Launch RViz for visualization
+roslaunch turtlebot_rviz_launchers view_navigation.launch &
+
+# Launch the pick_objects node
+rosrun pick_objects_node pick_objects_node
+
--- test_slam.sh
+++
@@ -1,9 +0,0 @@
-source devel/setup.bash
-xterm -e "roslaunch ./src/turtlebot_simulator/turtlebot_gazebo/launch/turtlebot_world.launch " &
-sleep 5
-xterm -e "roslaunch ./src/turtlebot_simulator/turtlebot_gazebo/launch/gmapping_demo.launch " &
-sleep 5
-xterm -e "roslaunch ./src/turtlebot_interactions/turtlebot_rviz_launchers/launch/view_navigation.launch " &
-sleep 5
-xterm -e "roslaunch ./src/turtlebot/turtlebot_teleop/launch/keyboard_teleop.launch " &
-sleep 5
